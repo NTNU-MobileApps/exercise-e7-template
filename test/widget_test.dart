588abaeb52792e8b074493b408581ea2e0e9f203
Girts Strazdins <girts.strazdins@gmail.com>
2022-10-17 14:35:32 +0200

    Exercise described

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,56 +1,73 @@
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  // TODO - no errors first
 
-  // TODO - action bar shows no numbers first
+  // TODO (1.1) - by default, no products in the cart - no number next to cart icon
 
-  // TODO - when trying to add a product with no size to the cart - show error
+  // TODO (2) No size selected by default
 
-  // TODO - Can add one M-size t-shirt to the cart:
-  //    - the button works
+  // TODO (3) no errors first
+
+  // TODO (4) count shows 1 by default
+
+  // TODO (5.1) minus button is disabled by default
+
+  // TODO (5) can press +: changes the count to 2
+  // TODO (5) can press + twice, count is 3, + amd - buttons enabled
+  // TODO (5.1) can't press - when count is 1
+  // TODO (5.2) can't press + when count is 10
+  // TODO (5) can press + 9 times, then - once, then + again. Count is 10
+  // TODO (5) can press +, then -. Count is 1, - button disabled
+
+  // TODO (6) The "Add to cart" button is enabled by default
+
+  // TODO (7.1.1) when trying to add a product with no size to the cart - show error
+
+  // TODO (7.2) Can add one M-size t-shirt to the cart:
+  //    - (6) the button works
   //    - no error shown
-  //    - action bar shows 1 after the product is added
+  //    - (1.2) action bar shows 1 after the product is added
 
-  // TODO - can add one L-size t-shirt to the cart
+  // TODO (7.2) Can add one L-size t-shirt to the cart
   //    - same conditions as previous test
 
-  // TODO - count shows 1 by default
 
-  // TODO - can press +: changes the count to 2, then to 3
-
-  // TODO - can add 3 L-size t-shirts to the cart
+  // TODO (7.2) Can add 3 L-size t-shirts to the cart
   //    - the button works
   //    - no error shown
   //    - action bar shows 3 after the product is added
 
-  // TODO - can add 2 L-size and then 3 L-size to the cart:
+  // TODO (7.2) Can add 2 L-size and then 3 L-size to the cart:
   //    - the button works
   //    - no error shown
   //    - action bar shows 5 after the products are added
 
-  // TODO - can add 2 L-size and then 3 S-size to the cart:
+  // TODO (7.2) can add 2 L-size and then 3 S-size to the cart:
   //    - the button works
   //    - no error shown
   //    - action bar shows 5 after the products are added
 
-  // TODO - Click to the cart icon opens shopping cart page
+  // TODO (8) Click to the cart icon opens shopping cart page
 
-  // TODO - by default, no products in the cart - the text is shown
+  // TODO (9) By default, the shopping cart page shows "No items in the cart"
 
-  // TODO - when one product is added, it is displayed in the cart
+  // TODO (10) when one product is added, it is displayed in the cart
 
-  // TODO - add 2 L-size and then 3 L-size to the cart. These must be visible correctly in the cart
+  // TODO (10) add 2 L-size and then 3 L-size to the cart. These must be visible correctly in the cart
 
-  // TODO - Delete icon visible when 1 item in the cart
+  // TODO (11) Delete icon visible when 1 item in the cart
 
-  // TODO - When pressing on the delete icon (for a single-item cart), the cart becomes empty
+  // TODO (11) When pressing on the delete icon (for a single-item cart), the cart becomes empty, "The cart is empty" text is shown
 
-  // TODO - When pressing on the delete icon on the 1st of 3 items (2xS shirts, 3xM shirts, 1x XL shirt), the first item disappears
+  // TODO (11) When pressing on the delete icon on the 1st of 3 items (2xS shirts, 3xM shirts, 1x XL shirt), the first item disappears
 
-  // TODO - When pressing on the delete icon on the 2nd of 3 items (2xS shirts, 3xM shirts, 1x XL shirt), the 2nd item disappears
+  // TODO (11, 1) When pressing on the delete icon on the 1st of 3 items (2xS shirts, 3xM shirts, 1x XL shirt), the first item disappears. When we go back to the product page, the number of items in the cart is updated.
 
-  // TODO - When pressing on the delete icon on the 3rd of 3 items (2xS shirts, 3xM shirts, 1x XL shirt), the 3rd item disappears
+  // TODO (11) When pressing on the delete icon on the 2nd of 3 items (2xS shirts, 3xM shirts, 1x XL shirt), the 2nd item disappears
 
-  // TODO - When pressing on the first and then 3rd delete icon, only the 2nd item remains in the cart
+  // TODO (11) When pressing on the delete icon on the 3rd of 3 items (2xS shirts, 3xM shirts, 1x XL shirt), the 3rd item disappears
+
+  // TODO (11) When pressing on the first and then 3rd delete icon, only the 2nd item remains in the cart
+
+  // TODO (11, 8) When adding three items to the cart, then deleting all of them, we get the "The cart is empty" text again
 }
